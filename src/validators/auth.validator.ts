e50a867c64fa8ffa3130830200e27ace1aca4348
Lib Martinito <lib.martinito@pm.me>
2023-02-25 21:13:09 +0800

    refactor: removed items in the validation chain

--- src/validators/auth.validator.ts
+++ src/validators/auth.validator.ts
@@ -2,8 +2,8 @@ import { body, ValidationChain } from 'express-validator'
 
 export const signupValidationRules = (): ValidationChain[] => {
   return [
-    body('email').exists().isEmail().bail().normalizeEmail(),
-    body('password').exists().isString().bail(),
+    body('email').exists().isEmail().normalizeEmail(),
+    body('password').exists().isString(),
     body('role').exists().custom((value) => value === 'ADMIN' || 'SUBSCRIBER')
   ]
 }
